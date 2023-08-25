1f744aa6f2dedec52a6d43b218fc60f0ed54272b
Lan Kim Ho <lan.ho@asnet.com.vn>
2023-08-25 14:27:07 +0700

    fixed

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-// server.use(cors())
+server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const PORT = 8000
 
 server.listen(PORT, () => {
