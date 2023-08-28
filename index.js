7dc9edf38ae278a20790e239f72a2246a2ab5d1e
Lan Kim Ho <lan.ho@asnet.com.vn>
2023-08-28 15:40:13 +0700

    moved db to tmp folder

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
-const express = require('express')
 
-const server = express()
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'tmp/db.json'))
 const middlewares = jsonServer.defaults()
-
 server.use(cors())
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 server.use(jsonServer.bodyParser)
-server.use('/api', jsonServer.router('/tmp/db.json'));
 server.use(middlewares)
+server.use(router)
 const PORT = 8000
 
 server.listen(PORT, () => {
