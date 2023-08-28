841d1cd75f07cecb31a8fb5fde8219f51f80a390
Lan Kim Ho <lan.ho@asnet.com.vn>
2023-08-28 14:33:01 +0700

    updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use(
 );
 server.options('*', cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "https://keen-semolina-49781b.netlify.app"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
